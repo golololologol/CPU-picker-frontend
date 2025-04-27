53eeb8ce6f7c58b9e341a994b037e61b3785dae0
golololologol <golololologol@gmail.com>
2025-04-28 02:35:13 +0300

    maybe this will work?

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => {
-  const apiBase = process.env.VITE_API_BASE_URL || '/api';
+  const apiBase = process.env.VITE_API_BASE_URL;
   return {
     plugins: [react()],
     define: { 'import.meta.env.VITE_API_BASE_URL': JSON.stringify(apiBase) },
